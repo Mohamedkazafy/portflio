c64182ef470b48269846f41f61ebaa05f803daec
mohamed <mohamedkazafy78@gmail.com>
2024-06-02 22:31:56 +0300

    tech animation

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -4,27 +4,64 @@ import {TbBrandNextjs} from "react-icons/tb"
 import {SiMongodb} from "react-icons/si"
 import {DiRedis} from "react-icons/di"
 import {FaNodeJs} from "react-icons/fa"
+import { motion } from "framer-motion"
+const iconVariant = (duration) => ({
+    initial: {y:-10},
+    animate:{
+        y : [10,-10],
+        transition:{
+            duration: duration,
+            ease: "linear",
+            repeat: Infinity,
+            repeatType: "reverse"
+        },
+    }
+})
 const Tech = () => {
   return (
     <div className=' border-b border-neutral-900 pb-24'>
         <h2 className=' my-20 text-center text-4xl'>Technologies</h2>
-        <div className=' flex flex-wrap items-center justify-center gap-4'>
-            <div className=' rounded-2xl border-4 border-neutral-800 p-4'>
+        <motion.div 
+        whileInView={{opacity:1,x:0}}
+        initial={{opacity:1,x:-100}}
+        transition={{duration:1.5}}
+        className=' flex flex-wrap items-center justify-center gap-4'>
+            <motion.div 
+            variants={iconVariant(2.5)}
+            initial="initial"
+            animate="animate"
+            className=' rounded-2xl border-4 border-neutral-800 p-4'>
                 <RiReactjsLine className=' text-7xl text-cyan-400'/>
-            </div>
-            <div className=' rounded-2xl border-4 border-neutral-800 p-4'>
+            </motion.div>
+            <motion.div 
+            variants={iconVariant(3)}
+            initial="initial"
+            animate="animate"
+            className=' rounded-2xl border-4 border-neutral-800 p-4'>
                 <TbBrandNextjs className=' text-7xl text-white'/>
-            </div>
-            <div className=' rounded-2xl border-4 border-neutral-800 p-4'>
+            </motion.div>
+            <motion.div 
+            variants={iconVariant(5)}
+            initial="initial"
+            animate="animate"
+            className=' rounded-2xl border-4 border-neutral-800 p-4'>
                 <SiMongodb className=' text-7xl text-green-800'/>
-            </div>
-            <div className=' rounded-2xl border-4 border-neutral-800 p-4'>
+            </motion.div>
+            <motion.div
+            variants={iconVariant(2)}
+            initial="initial"
+            animate="animate"
+            className=' rounded-2xl border-4 border-neutral-800 p-4'>
                 <DiRedis className=' text-7xl text-violet-700'/>
-            </div>
-            <div className=' rounded-2xl border-4 border-neutral-800 p-4'>
+            </motion.div>
+            <motion.div 
+            variants={iconVariant(6)}
+            initial="initial"
+            animate="animate"
+            className=' rounded-2xl border-4 border-neutral-800 p-4'>
                 <FaNodeJs className=' text-7xl text-green-400'/>
-            </div>
-        </div>
+            </motion.div>
+        </motion.div>
     </div>
   )
 }
