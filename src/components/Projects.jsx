30944785a15fed0b1e6f74a0f73f1d2597b4f74a
mohamed <mohamedkazafy78@gmail.com>
2024-06-02 22:42:51 +0300

    k

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {PROJECTS} from "../constants"
+import { motion } from "framer-motion"
 const Projects = () => {
   return (
     <div className=' border-b border-neutral-900 pb-4'>
@@ -7,10 +8,18 @@ const Projects = () => {
         <div>
             {PROJECTS.map((project,idx) => (
                 <div key={idx} className='mb-8 flex flex-wrap lg:justify-center'>
-                    <div className=' w-full lg:w-1/4'>
+                    <motion.div 
+                    whileInView={{opacity:1,x:0}}
+                    initial={{opacity:0,x:-100}}
+                    transition={{duration:1}}
+                    className=' w-full lg:w-1/4'>
                         <img src={project.image} alt={project.title} width={150} height={150} className=' mb-6 rounded'/>
-                    </div>
-                    <div className=' w-full max-w-xl lg:w-3/4'>
+                    </motion.div>
+                    <motion.div 
+                    whileInView={{opacity:1,x:0}}
+                    initial={{opacity:0,x:100}}
+                    transition={{duration:1}}
+                    className=' w-full max-w-xl lg:w-3/4'>
                         <h6 className=' mb-2 font-semibold'>{project.title}</h6>
                         <p className='mb-4 text-neutral-400'>{project.description}</p>
                         {project.technologies.map((tech,index) => (
@@ -18,7 +27,7 @@ const Projects = () => {
                               {tech}  
                             </span>
                         ))}
-                    </div>
+                    </motion.div>
                 </div>
             ))}
         </div>
