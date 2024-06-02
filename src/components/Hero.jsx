e158403b4e7ae24b8845569b36e421a958814bea
mohamed <mohamedkazafy78@gmail.com>
2024-06-02 22:08:00 +0300

    animate text hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,33 @@
 import React from 'react'
+import { delay, motion } from "framer-motion"
 import {HERO_CONTENT} from "../constants"
 import profilePic from '../assets/kevinRushProfile.png'
+const container = (delay) => ({
+    hidden: {x:-100, opacity:0},
+    visible:{
+        x:0,
+        opacity:1,
+        transition:{duration:0.5, delay:delay}
+    }
+})
 const Hero = () => {
   return (
     <div className=' border-b border-neutral-900 pb-4 lg:mb-35'>
         <div className=' flex flex-wrap'>
             <div className='w-full lg:w-1/2'>
                 <div className='flex flex-col items-center lg:items-start'>
-                    <h1 className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>Mohamed Mostafa</h1>
-                    <span className=' bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent'>Front End Developer</span>
-                    <p className=' my-2 max-w-xl py-5 font-light tracking-tighter'>{HERO_CONTENT}</p>
+                    <motion.h1 
+                    variants={container(0)}
+                    initial="hidden"
+                    animate="visible"
+                    className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>Mohamed Mostafa</motion.h1>
+                    <motion.span  variants={container(0.5)}
+                    initial="hidden"
+                    animate="visible"
+                    className=' bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent'>Front End Developer</motion.span>
+                    <motion.p className=' my-2 max-w-xl py-5 font-light tracking-tighter'  variants={container(1)}
+                    initial="hidden"
+                    animate="visible">{HERO_CONTENT}</motion.p>
                 </div>
             </div>
             <div className=' w-full lg:w-1/2 lg:p-8'>
