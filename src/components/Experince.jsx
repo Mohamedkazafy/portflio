5334dc150b4484a19ff1da0021f83e08734c48bd
mohamed <mohamedkazafy78@gmail.com>
2024-06-02 22:36:50 +0300

    Exp animation

--- src/components/Experince.jsx
+++ src/components/Experince.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {EXPERIENCES} from "../constants"
+import { motion } from "framer-motion"
 const Experince = () => {
   return (
     <div className='border-b border-neutral-900 pb-4'>
@@ -7,16 +8,24 @@ const Experince = () => {
         <div>
             {EXPERIENCES.map((exp, idx) => (
                 <div key={idx} className=' mb-8 flex flex-wrap lg:justifiy-center'>
-                    <div className=' w-full lg:w-1/4'>
+                    <motion.div 
+                    whileInView={{opacity:1,x:0}}
+                    initial={{opacity:0,x:-100}}
+                    transition={{duration:1}}
+                    className=' w-full lg:w-1/4'>
                         <p className=' mb-2 text-sm text-neutral-400'>{exp.year}</p>
-                    </div>
-                    <div className='w-full max-w-xl lg:w-3/4'>
+                    </motion.div>
+                    <motion.div 
+                    whileInView={{opacity:1,x:0}}
+                    initial={{opacity:0,x:100}}
+                    transition={{duration:1}}
+                    className='w-full max-w-xl lg:w-3/4'>
                         <h6 className='mb-2 font-semibold'>{exp.role} - <span className=' text-sm text-purple-100'>{exp.company}</span></h6>
                         <p className=' mb-4 text-neutral-400'>{exp.description}</p>
                         {exp.technologies.map((tech,index) => (
                             <span key={index} className='mr-2 mt-4 rounded bg-neutral-900 py-1 px-2 text-sm font-medium text-purple-800'>{tech}</span>
                         ))}
-                    </div>
+                    </motion.div>
                 </div>
             ) )}
         </div>
